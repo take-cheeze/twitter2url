0ed153ae9e37fe0941aefc4272e36368e8c2f913
take_cheeze <takechi101010@gmail.com>
2011-08-18 17:01:00 +0900

    remove some excluding domains

--- open_urls.sh
+++ open_urls.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-for i in $(cat *.txt | sort | uniq | grep -v "shindanmaker.com" | grep -v "foursquare.com" | grep -v "30thou.com" | grep -v "amazon.co.jp" | grep -v "nicovideo.jp/" | grep -v "youtube.com" | grep -v "gigazine.net"); do open -a "Google Chrome Canary" $i; done
+for i in $(cat *.txt | sort | uniq | grep -v "shindanmaker.com" | grep -v "foursquare.com" | grep -v "30thou.com"); do open -a "Google Chrome Canary" $i; done
