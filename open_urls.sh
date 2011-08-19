70c36bec678cb039ad4bfc257bae4e2853084cab
take_cheeze <takechi101010@gmail.com>
2011-08-19 13:09:13 +0900

    rewrite open_urls.sh

--- open_urls.sh
+++ open_urls.sh
@@ -1,3 +1,36 @@
 #!/usr/bin/env sh
 
-for i in $(cat *.txt | sort | uniq | grep -v "shindanmaker.com" | grep -v "foursquare.com" | grep -v "30thou.com"); do open -a "Google Chrome Canary" $i; done
+cat *.txt | sort | uniq > tmp.txt
+
+# exclude some pattern
+for i in \
+    "30thou\.com" \
+    "agilecat\.tumblr\.com" \
+    "ataru-mix\.tumblr\.com" \
+    "fanfou\.com" \
+    "firela\.biz" \
+    "foursquare\.com" \
+    "hexe\.tumblr\.com" \
+    "motomocomo\.tumblr\.com" \
+    "odiva\.jp" \
+    "paper\.li" \
+    "shibats\.tumblr\.com" \
+    "shindanmaker\.com" \
+    ;
+do
+    grep -v $i tmp.txt > tmp_dst.txt
+    cat tmp_dst.txt > tmp.txt
+done
+
+# open with Google Chrome Canary
+counter=0
+interval=30
+
+for i in $(cat tmp.txt); do
+    if [ `expr $counter % $interval` = 0 ] ; then
+        echo "Open item $counter - $(expr $counter + $interval) (`wc tmp.txt`):"
+        read
+    fi
+    open -a "Google Chrome Canary" $i
+    counter=$(expr $counter + 1)
+done
